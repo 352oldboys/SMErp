2c517d7e29833971d7ecd3142277e282fecb9c26
user1 <user1@DESKTOP-3P8K2VA>
2020-07-20 17:11:03 +0900

    dsf

---
+++ Oldboys/src/main/java/com/kh/erp/finance/sales/controller/SalesController.java
@@ -0,0 +1,11 @@
+package com.kh.erp.finance.sales.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SalesController {
+
+	@RequestMapping("/purchase.do")
+	public String purchase() {
+		
+	}
+}
